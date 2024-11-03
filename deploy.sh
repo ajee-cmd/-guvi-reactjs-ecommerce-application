5495ab5826ebd090be4ef09a255d85b585142c41
ajee-cmd <priyaethiraj1298@gmail.com>
2024-11-03 21:45:32 +0530

    bash script added

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,12 @@
 
 # Variables
 ENV=$1
-IMAGE_NAME="priya2908/$ENV-guvi"
-TAG=$2 # You can set this to a dynamic version if needed
 DOCKER_PASSWORD=$3
 
 # Run the Docker container
 docker login --username priya2908 --password $DOCKER_PASSWORD
 echo "Running Docker container..."
 #docker run -d -p 80:80 --name react-app $priya2908/dev-guvi:$
-docker compose up -d
+IMAGE_NAME="priya2908/$ENV-guvi" TAG=$2 DOCKER_PASSWORD=$3 docker compose up -d
 
 echo "Application deployed successfully."
