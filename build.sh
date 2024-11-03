bcf60773a1937e13704522951a81274bf935746e
ajee-cmd <priyaethiraj1298@gmail.com>
2024-11-03 19:51:59 +0530

    bash script added

--- build.sh
+++ build.sh
@@ -4,12 +4,14 @@
 ENV=$1
 IMAGE_NAME="priya2908/$ENV-guvi"
 TAG=$2 # You can set this to a dynamic version if needed
+DOCKER_PASSWORD=$3
 
 # Build the Docker image
 echo "Building Docker image..."
 docker build -t $IMAGE_NAME:$TAG .
 
 # Push the Docker image to Docker Hub or your registry
+docker login --username priya2908 --password $DOCKER_PASSWORD
 echo "Pushing Docker image..."
 docker push $IMAGE_NAME:$TAG
 
--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,12 @@
 ENV=$1
 IMAGE_NAME="priya2908/$ENV-guvi"
 TAG=$2 # You can set this to a dynamic version if needed
+DOCKER_PASSWORD=$3
 
 # Run the Docker container
+docker login --username priya2908 --password $DOCKER_PASSWORD
 echo "Running Docker container..."
-#docker run -d -p 80:80 --name react-app $priya2908/dev-guvi:$latest
+#docker run -d -p 80:80 --name react-app $priya2908/dev-guvi:$
 docker compose up -d
 
 echo "Application deployed successfully."
